b34b518cf4977c611b4abc49e7954d43c064e287
babacare <wanguiwainaina193@gmail.com>
2024-09-25 14:51:05 +0300

    Add files via upload

---
+++ interest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+    {
+        int a, b;
+        printf("Enter values a and b");
+        scanf("%d%d", &a, &b);
+        c= a+b;
+        printf("sum", a,b c);
+        return 0;
+    }
